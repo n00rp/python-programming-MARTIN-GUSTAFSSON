5935331bfc2e4ffbe6e21800a552ade1f4be2482
Martin Gustafsson <martin.gustafsson@iths.se>
2024-09-12 12:38:11 +0200

    Uppladdning av uppgift

--- Exercises/test.py
+++ Exercises/test.py
@@ -1,25 +1,34 @@
-import random
+import random as rng
+user_score = 0
 
-def number_guessing_game():
-    number = random.randint(1, 10)
+print(f"Please choose a difficulty level.")
+print(f"1 - Easy")
+print(f"2 - Medium")
+print(f"3 - Hard")
 
-    player_name = input("Hello, What's your name? ")
-    number_of_guesses = 0
+difficulty = int(input(f"Choose a difficulty setting for this excercise (1-3): "))
 
-    print("Okay, " + player_name + "! I am guessing a number between 1 and 10:")
 
-    while number_of_guesses < 5:
-        guess = int(input())
-        number_of_guesses += 1
+while True:
 
-        if guess < number:
-            print('Your guess is too low.')
-        elif guess > number:
-            print('Your guess is too high.')
-        else:
-            break
+    if difficulty == "1":
+        x = rng.randint(2, 2)
+        y = rng.randint(2, 2)
+    if difficulty == "2":
+        x = rng.randint(3, 5)
+        y = rng.randint(3, 5)
+    if difficulty == "3":
+        x = rng.randint(1, 10)
+        y = rng.randint(1, 10)
 
-    if guess == number:
-        print('Congratulations, ' + player_name + '! You guessed the number in ' + str(number_of_guesses) + ' tries!')
+    user_input = int(input(f"What is {x} x {y}?. "))
+    if user_input == x*y:
+        print(f"Good work")
+        user_score += 1    
     else:
-        print('Sorry, ' + player_name + ', you did not guess the number. The number was ' + str(number))
+        print(f"{x*y} is the correct answer")
+    print(f"Your score is now {user_score}")    
+    play_again = input(f"Do you want to play again? (y / n): ")
+    if play_again != "y":
+        print(f"Thank you for playing, have a nice day!")
+        break
