67ac62e492bbceeb4e6d669cebb3da9e2f4a5fe5
Martin Gustafsson <martin.gustafsson@iths.se>
2024-09-25 08:57:46 +0200

    Uppladdning av LAB 2 - uppdatering

--- Labs/lab_2.py
+++ Labs/lab_2.py
@@ -1,8 +1,29 @@
 
-
-path = "../Data/datapoint.txt"
+path = "Data/datapoints.txt"
 
 with open(path, "r") as f:
     text = f.read()
 
 print(repr(text))
+
+import re
+
+quotes = []
+
+with open(path, "r") as f_read, open("Data/datapoints_clean.txt", "w") as f_write:
+    f_write.write("Pokémon list\n\n")
+    for quote in f_read:
+        #quote = quote.strip(" \n")
+        #quote = re.sub(r" +", " ", quote)
+        if quote != "":
+            f_write.write(f"{quote}\n")
+
+with open("Data/datapoints_clean.txt", "r") as type:
+    pokemon_type = None
+
+
+
+
+        
+            
+                             
